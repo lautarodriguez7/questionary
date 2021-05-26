4a6539d33c92a67bc5f81d3a695bca5fc3ae37dc
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2021-05-26 18:16:02 -0300

    adding in folder components questionary.jsx where will be the child component in the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Questionary from './components/questionary'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Questionary />
     </div>
   );
 }
---
+++ src/components/questionary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const questionary = () => {
+    return (
+        <div className='questionary'>
+            <h3>Probando</h3>
+        </div>
+    )
+}
+
+export default questionary
